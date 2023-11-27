b197558ec643b55f7c7b863345e75c5b8128111f
scrapasd <asdmail045@gmail.com>
2023-11-27 18:13:32 +0530

    in

---
+++ connection.js
@@ -0,0 +1,30 @@
+const io = require("socket.io")(server, {
+  path: "/pathToConnection"
+});
+let users = {};
+io.on("connection", (socket) => {
+  let userId = socket.handshake.query.userId; // GET USER ID
+
+  // CHECK IS USER EXHIST
+  if (!users[userId]) users[userId] = [];
+
+  // PUSH SOCKET ID FOR PARTICULAR USER ID
+  users[userId].push(socket.id);
+
+  // USER IS ONLINE BROAD CAST TO ALL CONNECTED USERS
+  io.sockets.emit("online", userId);
+
+  // DISCONNECT EVENT
+  socket.on("disconnect", (reason) => {
+    // REMOVE FROM SOCKET USERS
+    _.remove(users[userId], (u) => u === socket.id);
+    if (users[userId].length === 0) {
+      // ISER IS OFFLINE BROAD CAST TO ALL CONNECTED USERS
+      io.sockets.emit("offline", userId);
+      // REMOVE OBJECT
+      delete users[userId];
+    }
+
+    socket.disconnect(); // DISCONNECT SOCKET
+  });
+});
